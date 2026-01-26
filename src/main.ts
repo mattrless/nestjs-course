e78797e1cac5af63569fe28393525b4a5c5366f1
mattrless <carlosdanstd@gmail.com>
2026-01-26 09:36:26 -0600

    helmet, cors and build
    Package: pnpm i --save helmet
    -We can use cors in case we only have a web to allow only that web.
    -Error in start:prod (after build), the command was looking in the wrong path.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,11 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory, {
     jsonDocumentUrl: 'swagger/json',
+  });
+
+  app.use(helmet());
+  app.enableCors({
+    origin: '*',
   });
 
   await app.listen(process.env.PORT ?? 3000);
